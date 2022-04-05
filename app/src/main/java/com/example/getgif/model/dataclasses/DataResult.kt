fc65d4853c1a05cfa2a64177a830a30ac3e98b5e
OfirKosto <59827963+OfirKosto@users.noreply.github.com>
2022-04-05 15:26:31 +0300

    minor changes

--- app/src/main/java/com/example/getgif/model/dataclasses/DataResult.kt
+++ app/src/main/java/com/example/getgif/model/dataclasses/DataResult.kt
@@ -3,5 +3,5 @@ package com.example.getgif.model.dataclasses
 import com.google.gson.annotations.SerializedName
 
 data class DataResult(
-    @SerializedName("data") val gifsList: List<Gif>?
+    @SerializedName("data") val gifsList: List<Gif>? = null
 )
--- app/src/main/java/com/example/getgif/model/dataclasses/Gif.kt
+++ app/src/main/java/com/example/getgif/model/dataclasses/Gif.kt
@@ -3,6 +3,6 @@ package com.example.getgif.model.dataclasses
 import com.google.gson.annotations.SerializedName
 
 data class Gif(
-    @SerializedName("id") val id: String,
-    @SerializedName("url") val url: String,
+    @SerializedName("id") val id: String? = null,
+    @SerializedName("url") val url: String? = null,
 )
