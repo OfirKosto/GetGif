048965700b00443426afb6b118f517ac621b5516
OfirKosto <59827963+OfirKosto@users.noreply.github.com>
2022-04-07 13:25:13 +0300

    Working on GifFragment

--- app/src/main/java/com/example/getgif/model/GifsRepository.kt
+++ app/src/main/java/com/example/getgif/model/GifsRepository.kt
@@ -8,36 +8,10 @@ object GifsRepository {
     private const val RATING: String  = "g"
     private const val LIMIT: Int = 40
 
-//    private fun createMyCallback(listener: IResponseListener): Callback<DataResult> = object : Callback<DataResult> {
-//        override fun onResponse(call: Call<DataResult>, response: Response<DataResult>) {
-//            if (response.isSuccessful) {
-//                if (response.body() != null) {
-//                    if(response.body()!!.gifsList != null)
-//                    {
-//                        listener.getResponse(response.body()!!.gifsList, true)
-//                    }
-//                    else
-//                    {
-//                        listener.getResponse(listOf<Gif>(), true)
-//                    }
-//                }
-//            } else {
-//                listener.getResponse(null, false)
-//            }
-//        }
-//
-//        override fun onFailure(call: Call<DataResult>, t: Throwable) {
-//            listener.getResponse(null, false)
-//        }
-//    }
-//
-//    suspend fun getTrendingGifs(listener: IResponseListener) = GifApiUtil.getTrendingGifs(createMyCallback(listener))
-//
-//
-//
-//    suspend fun getGifsByName(nameToSearch: String, listener: IResponseListener) = GifApiUtil.getGifsByName(nameToSearch, createMyCallback(listener))
-
     suspend fun getTrendingGifs() = GifApiUtil.retroService.getTrendingGifs(LIMIT, RATING, API_KEY)
 
     suspend fun getGifsByName(nameToSearch: String) = GifApiUtil.retroService.getGifsByName(nameToSearch, LIMIT, RATING, API_KEY)
+
+
+
 }
--- app/src/main/java/com/example/getgif/view/GifScreenFragment.kt
+++ app/src/main/java/com/example/getgif/view/GifScreenFragment.kt
@@ -1,20 +1,34 @@
 package com.example.getgif.view
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.transition.Transition
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DecodeFormat
+import com.bumptech.glide.load.resource.gif.GifDrawable
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.CustomTarget
 import com.example.getgif.R
+import java.io.File
 
 class GifScreenFragment : Fragment() {
 
 
     private lateinit var viewModel: GifScreenViewModel
     private lateinit var gifImageView: ImageView
-
+    private lateinit var shareButton: Button
+    private lateinit var saveButton: Button
+    private lateinit var url: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +38,18 @@ class GifScreenFragment : Fragment() {
 
         initUI(viewRoot)
 
+        val bundle = arguments
+        if(bundle != null)
+        {
+            if(bundle.getString("url") != null)
+            {
+                url = bundle.getString("url")!!
+                Glide.with(viewRoot.context).load(url).into(gifImageView)
+            }
+        }
+
+
+
 
 
         return viewRoot
@@ -32,14 +58,91 @@ class GifScreenFragment : Fragment() {
 
 
     private fun initUI(viewRoot: View) {
-        gifImageView = viewRoot.findViewById<ImageView>(R.id.gif_fragment_imageView)
+        gifImageView = viewRoot.findViewById(R.id.gif_fragment_imageView)
 
-        val bundle: Bundle? = arguments
-        if(bundle != null)
-        {
-            Glide.with(viewRoot.context).load(bundle.getString("url")).into(gifImageView)
-        }
 
+
+
+        shareButton = viewRoot.findViewById(R.id.gif_fragment_share_gif_btn)
+        shareButton.setOnClickListener(object :View.OnClickListener{
+            override fun onClick(p0: View?) {
+//                Glide.with(viewRoot).asBitmap().load(url)
+//                    .apply(RequestOptions()).format(DecodeFormat.PREFER_ARGB_8888)
+//                    .into(CustomTarget<Bitmap>() {
+//                        @Override
+//                        fun onResourceReady(resource: Bitmap , transition: Transition<? super Bitmap>) {
+//
+//                        }
+//
+//                        @Override
+//                        fun onLoadCleared( placeholder: Drawable) {
+//
+//                        }
+//                    })
+
+            }
+        })
+        shareButton.setOnClickListener(object :View.OnClickListener{
+            override fun onClick(p0: View?) {
+                shareImage()
+            }
+
+        })
+
+        saveButton = viewRoot.findViewById(R.id.gif_fragment_save_gif_btn)
+        saveButton.setOnClickListener(object :View.OnClickListener{
+            override fun onClick(p0: View?) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
+
+    private fun shareImage(){
+
+
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "image/*"
+        intent.putExtra(Intent.EXTRA_STREAM, url)
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+
+        startActivity(Intent.createChooser(intent, resources.getString(R.string.share_via)))
     }
 
 }
+
+//                Glide.with(viewRoot).asBitmap().load(url)
+//                    .apply(RequestOptions()).format(DecodeFormat.PREFER_ARGB_8888)
+//                    .into(CustomTarget<Bitmap>() {
+//                        @Override
+//                        fun onResourceReady(resource: Bitmap , transition: Transition<? super Bitmap>) {
+//
+//                        }
+//
+//                        @Override
+//                        fun onLoadCleared( placeholder: Drawable) {
+//
+//                        }
+//                    })
+
+// contentUri: Uri?
+//        val byteBuffer = (gifImageView.drawable as GifDrawable).buffer
+//        val location = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "" + resources.getString(R.string.app_name))
+//        val gifFile = File( location +"test.gif")
+//
+//        val output = FileOutputStream(gifFile)
+//        val bytes = ByteArray(byteBuffer.capacity())
+//        (byteBuffer.duplicate().clear() as ByteBuffer).get(bytes)
+//        output.write(bytes, 0 ,bytes.size)
+//        output.close()
+
+//        val customTarget: CustomTarget<File> = CustomTarget<File>(222,222){
+//            @Override
+//            fun onResourceReady(resource: Bitmap , transition: Transition<File>) {
+//
+//            }
+//
+//            @Override
+//            fun onLoadCleared( placeholder: Drawable) {
+//
+//            }
+//        }
